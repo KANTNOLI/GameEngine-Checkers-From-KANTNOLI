166d30683a38019080b6307ae81de99e0e98b2c1
KANTNOLI <zahargysev02@gmail.com>
2024-12-21 06:46:53 +0300

    0.6.6 Add security on DefaultViEnConfig +- 80%

--- client/js/Engine/VisualEngineConfigs/DefaultViEnConfig.js
+++ client/js/Engine/VisualEngineConfigs/DefaultViEnConfig.js
@@ -1,24 +1,45 @@
 import * as THREE from "three";
 
-export const DefaultViEnConfig = () => {
-  const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    precision: "lowp",
-    powerPreference: "high-performance",
-    depth: true,
-  });
-  //antialias - Сглаживание
-  //precision - точность расчётов шейдеров  / lowp   mediump   highpы
-  //powerPreference - уровень производительности для рендерера    high-performance  default  low-power
-  //depth - буфер глубины 
+//antialias - Сглаживание
+//precision - точность расчётов шейдеров  / lowp   mediump   highpы
+//powerPreference - уровень производительности для рендерера    high-performance  default  low-power
+//depth - буфер глубины
 
+export const DefaultViEnConfig = (
+  quality = {
+    antialias: true,
+    precision: "mediump",
+    powerPrfrnc: "default",
+    depth: true,
+    shadowOn: true,
+    shadowMap: "normal",
+  }
+) => {
   
+  const renderer = new THREE.WebGLRenderer({
+    antialias: quality.antialias,
+    precision: quality.precision,
+    powerPreference: quality.powerPrfrnc,
+    depth: quality.depth,
+  });
+
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.shadowMap.enabled = true;
-  //BasicShadowMap low
-  //PCFShadowMap norm
-  //PCFSoftShadowMap super
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  if (shadowOn) {
+    renderer.shadowMap.enabled = true;
+    switch (shadowMap) {
+      case "low":
+        renderer.shadowMap.type = THREE.BasicShadowMap;
+        break;
+      case "hard":
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        break;
+      default:
+        renderer.shadowMap.type = THREE.PCFShadowMap;
+        break;
+    }
+  }
+
   document.body.appendChild(renderer.domElement);
 
   return renderer;
