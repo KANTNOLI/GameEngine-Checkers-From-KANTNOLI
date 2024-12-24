8075e9fe9645aae9ab2e0124d4dfdc13db4bdd0d
KANTNOLI <zahargysev02@gmail.com>
2024-12-24 23:59:34 +0300

    0.7.3.1 add

--- client/js/Engine/Lighting/SpotLightCfg.js
+++ client/js/Engine/Lighting/SpotLightCfg.js
@@ -1,19 +1,41 @@
 import * as THREE from "three";
 
-export const SpotLightCfg = (scene, ) => {
-    const light = new THREE.SpotLight(0x00ff00, 1); // Зеленый свет с интенсивностью 1
-    light.position.set(100, 100, 100);
-    scene.add(light);
-    
-    // light.color.set(0xffa500); // Изменение цвета света
-    // light.intensity = 2; // Изменение интенсивности
-    light.distance = 300; // Изменение максимального расстояния света
-    light.angle = Math.PI / 4; // Изменение угла освещения конус
-    light.penumbra = 0.1; // Изменение полутона типо где меньше света 
-    light.decay = 2; // Изменение коэффициента затухания света
+export const SpotLightCfg = (
+  scene,
+  params = {
+    color: 0xffffff,
+    intensity: 1,
+  },
+  position = {
+    x: 1,
+    y: 1,
+    z: 1,
+  },
+  lighting = {
+    distance: 300,
+    angle: Math.PI / 4,
+    penumbra: 0.1,
+    decay: 2,
+  },
+  shadows = {
+    cast: true,
+    bias: 0.0001,
+    mapSize: 512,
+  }
+) => {
+  const light = new THREE.SpotLight(params.color, params.intensity); // Зеленый свет с интенсивностью 1
+  light.position.set(position.x, position.y, position.z);
+  scene.add(light);
 
-    light.castShadow = true; // Включение теней
-    light.shadow.bias = 0.0001; // Пример значения для shadow.bias
-    light.shadow.mapSize.width = 2048; // Установка размера карты теней
-    light.shadow.mapSize.height = 2048; // Установка размера карты теней
+  // light.color.set(0xffa500); // Изменение цвета света
+  // light.intensity = 2; // Изменение интенсивности
+  light.distance = lighting.distance; // Изменение максимального расстояния света
+  light.angle = lighting.angle; // Изменение угла освещения конус
+  light.penumbra = lighting.penumbra; // Изменение полутона типо где меньше света
+  light.decay = lighting.decay; // Изменение коэффициента затухания света
+
+  light.castShadow = shadows.cast; // Включение теней
+  light.shadow.bias = shadows.bias; // Пример значения для shadow.bias
+  light.shadow.mapSize.width = shadows.mapSize; // Установка размера карты теней
+  light.shadow.mapSize.height = shadows.mapSize; // Установка размера карты теней
 };
