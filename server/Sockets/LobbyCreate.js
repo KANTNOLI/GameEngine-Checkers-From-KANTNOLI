5aa91fb41ec6744aff20c7973e0633303a3841be
KANTNOLI <zahargysev02@gmail.com>
2025-01-07 04:43:21 +0300

    v1.5.1 Secure LobbyCreate

--- server/Sockets/LobbyCreate.js
+++ server/Sockets/LobbyCreate.js
@@ -11,13 +11,19 @@ const LobbyCreate = (io, socket, users, rooms) => {
     // }
     socket.join(roomParam.room);
 
+    users[socket.id].room = users[socket.id].room || Object.create(null);
+    users[socket.id].game = users[socket.id].game || Object.create(null);
+
     users[socket.id].nickname = roomParam.owner;
     users[socket.id].room.owner = true;
     users[socket.id].room.roomID = roomParam.room;
     users[socket.id].game.side = roomParam.side;
 
-    rooms[roomParam.room] = roomParam;
-    rooms[roomParam.room].serverOSave = users[socket.id];
+    rooms[roomParam.room] = Object.assign(Object.create(null), roomParam);
+    rooms[roomParam.room].serverOSave = Object.assign(
+      Object.create(null),
+      users[socket.id]
+    );
 
     io.emit("newRoom", roomParam);
   });
